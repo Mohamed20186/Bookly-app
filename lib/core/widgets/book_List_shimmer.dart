d455edeaec86a708346821c1edcad70ecf58b228
mohamed ahmed <m.ahmed20201863@gmail.com>
2026-04-06 18:57:11 +0200

    Add shimmer effect for loading state in BookListView

---
+++ lib/core/widgets/book_List_shimmer.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class BookListShimmer extends StatelessWidget {
+  const BookListShimmer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 250,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 6,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Shimmer.fromColors(
+              baseColor: Colors.grey.shade300,
+              highlightColor: Colors.grey.shade100,
+              child: Container(
+                width: 120,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
